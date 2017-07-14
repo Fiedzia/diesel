12af55d64deba57271c09ac3f330abfe25d1ef07
Maciej <fiedzia@gmail.com>
2017-07-14 04:44:16 +0100

    use parse_bytes, remove unnecessary comments

--- diesel/src/mysql/types/numeric.rs
+++ diesel/src/mysql/types/numeric.rs
@@ -4,7 +4,6 @@ pub mod bigdecimal {
 
     use std::error::Error;
     use std::io::prelude::*;
-    use std::str::{self, FromStr};
 
     use mysql::{Mysql, MysqlType};
 
@@ -14,19 +13,17 @@ pub mod bigdecimal {
 
     impl ToSql<types::Numeric, Mysql> for BigDecimal {
         fn to_sql<W: Write>(&self, out: &mut W) -> Result<IsNull, Box<Error + Send + Sync>> {
-            // Mysql representats decimal type as char[]
-            write!(out, "{}", *self).map(|_| IsNull::No).map_err(|e| e.into())
+            write!(out, "{}", *self)
+                .map(|_| IsNull::No)
+                .map_err(|e| e.into())
         }
     }
 
     impl FromSql<types::Numeric, Mysql> for BigDecimal {
         fn from_sql(bytes: Option<&[u8]>) -> Result<Self, Box<Error+Send+Sync>> {
-            //_ numeric
-            match str::from_utf8(not_none!(bytes)) {
-                Ok(s) => BigDecimal::from_str(s)
-                            .map_err(|e| e.into()),
-                Err(e) => Err(Box::new(e) as Box<Error + Send + Sync>)
-            }
+            let bytes = not_none!(bytes);
+            BigDecimal::parse_bytes(bytes, 10)
+                .ok_or(Box::from(format!("{:?} is not valid decimal number ", bytes)))
        }
     }
 
